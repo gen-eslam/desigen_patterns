84f61a6d8cc692fbbb231db7b1290dbef5035aa5
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-08-23 15:24:15 +0200

    ObjectPool

---
+++ src/ObjectPool/App.java
@@ -0,0 +1,13 @@
+package ObjectPool;
+
+public class App {
+    public static void main(String[] args) {
+        ObjectPool robotsPool = new RobotsPool();
+        Robot firstRobot = (Robot) robotsPool.create();
+        Robot secondRobot = (Robot) robotsPool.create();
+
+        robotsPool.checkOut(firstRobot);
+
+        Robot thirdRobot = (Robot) robotsPool.checkIn();
+    }
+}
