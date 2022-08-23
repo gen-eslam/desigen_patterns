24408190463bd810d2258cbf5a256faeadc7e36b
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-08-23 13:49:50 +0200

    SingletonPattern

---
+++ src/SingletonPattern/SingletonLogger.java
@@ -0,0 +1,47 @@
+package SingletonPattern;
+
+public class SingletonLogger {
+    private static SingletonLogger instance = new SingletonLogger();
+    private SingletonLogger(){}
+// synchronized ==>>for multithreaded
+    public static synchronized SingletonLogger getInstance() {
+        return instance;
+    }
+    public void logMessageStart(){
+        System.out.println("Start message is logged");
+    }
+    public void logMessageStop(){
+        System.out.println("Stop message is logged");
+    }
+}
+/*      lazy initialize
+* //=>>>>> "memory efficient"
+*
+*   private static SingletonLogger instance;
+*   public static synchronized  SingletonLogger getInstance() {
+*        if(instance ==null){
+*             instance = new SingletonLogger();
+*           }
+        return instance;
+         }
+         public void logMessageStart(){
+        System.out.println("Start message is logged");
+    }
+    public void logMessageStop(){
+        System.out.println("Stop message is logged");
+    }
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
---
+++ src/SingletonPattern/main.java
@@ -0,0 +1,11 @@
+package SingletonPattern;
+
+public class main {
+    public static void main(String[] args)
+    {
+        SingletonLogger singletonLogger = SingletonLogger.getInstance();
+        singletonLogger.logMessageStart();
+        singletonLogger.logMessageStop();
+
+    }
+}
