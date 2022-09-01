6d0ce4272bed7dd6ca7ff85947dc896998325840
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-09-01 20:41:25 +0200

    strategy pattern

---
+++ src/Behavioral_Patterns/strategy_pattern/MultiplyOperation.java
@@ -0,0 +1,8 @@
+package Behavioral_Patterns.strategy_pattern;
+
+public class MultiplyOperation implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
