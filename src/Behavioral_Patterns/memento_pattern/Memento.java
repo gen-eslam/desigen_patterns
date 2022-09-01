322295408365befe481ed2c7e4478bce92b0af3a
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-09-01 18:29:43 +0200

    memento pattern

---
+++ src/Behavioral_Patterns/memento_pattern/Memento.java
@@ -0,0 +1,14 @@
+package Behavioral_Patterns.memento_pattern;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
+
---
+++ src/Behavioral_Patterns/memento_pattern/Originator.java
@@ -0,0 +1,21 @@
+package Behavioral_Patterns.memento_pattern;
+
+public class Originator {
+    private String state;
+
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+    public Memento storeStateToMemento(){
+        return new Memento(state);
+    }
+
+    public void retrieveStateFromMemento(Memento memento){
+        state = memento.getState();
+    }
+}
